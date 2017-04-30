9a0713f88e78c650a81a40dec349238077a694e8
BrittyB <BenjaminBritcliffe@outlook.com>
2017-04-30 16:42:10 +0100

    Add files via upload

---
+++ App_Data/dbo.payment.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[payment] (
+    [orderid]     NUMERIC (18)  IDENTITY (1, 1) NOT NULL,
+    [paymode]     VARCHAR (50)  NULL,
+    [cardno]      VARCHAR (50)  NULL,
+    [expirydate]  VARCHAR (50)  NULL,
+    [holdername]  VARCHAR (50)  NULL,
+    [address]     VARCHAR (MAX) NULL,
+    [tc]          VARCHAR (50)  NULL,
+    [name]        VARCHAR (50)  NULL,
+    [email]       VARCHAR (50)  NULL,
+    [sessionid]   VARCHAR (50)  NULL,
+    CONSTRAINT [PK_payment] PRIMARY KEY CLUSTERED ([orderid] ASC)
+);
+
---
+++ App_Data/dbo.signuptable.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[signuptable] (
+    [name]     VARCHAR (50) NULL,
+    [username] VARCHAR (50) NOT NULL,
+    [pass]     VARCHAR (50) NULL,
+    [mob]      NUMERIC (18) NULL,
+    [co]       VARCHAR (50) NULL,
+    [gen]      VARCHAR (50) NULL,
+    [dob]      VARCHAR (50) NULL,
+    [usertype] VARCHAR (50) NULL,
+    CONSTRAINT [PK_signuptable] PRIMARY KEY CLUSTERED ([username] ASC)
+);
+
